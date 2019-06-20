4dd0c40ebedd6170ab3374b0500e0ac244aded80
Iza Piotrowska <elpi2406@gmail.com>
2019-06-21 00:43:07 +0200

    added personList

--- src/sample/ControllerPeopleList.java
+++ src/sample/ControllerPeopleList.java
@@ -1,16 +1,22 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import polaczenie.KlasaPolaczenie;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
 
 public class ControllerPeopleList {
     public TableView tab;
@@ -30,13 +36,14 @@ public class ControllerPeopleList {
             e.printStackTrace();
             System.out.println("Blad polecenia sql");
         }
-        while(rs.next()){
+
+        while(rs.next()) {
             int id = rs.getInt(1);
             String imie = rs.getString(2);
             String nazwisko = rs.getString(3);
             int telefon = rs.getInt(4);
-            //peopleList.setText(peopleList.getText() + "\n" + id + " " + imie +" "+nazwisko+" " + telefon);
-
+            Person person = new Person(id, imie, nazwisko, telefon);
+            tab.getItems().add(person);
         }
 
     }
---
+++ src/sample/Person.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Person {
+    public int getId() {
+        return id.get();
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public int getPhone() {
+        return phone.get();
+    }
+
+    public Person(int id,String firstName, String lastName, int phone)
+    {
+        this.id= new SimpleIntegerProperty(id);
+        this.firstName= new SimpleStringProperty(firstName);
+        this.lastName=new SimpleStringProperty(lastName);
+        this.phone=new SimpleIntegerProperty(phone);
+    }
+
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty firstName;
+    private SimpleStringProperty lastName;
+    private SimpleIntegerProperty phone;
+
+
+}
